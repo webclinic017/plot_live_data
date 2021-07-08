aa9a15be9e83b950cd55213abc3fbc9c9cc9aeed
striker9921 <striker0120li@gmail.com>
2021-07-08 14:42:22 +0800

    Upload changes to bokeh_plot.py
    bokeh proved to be a much more suited library than matplotlib. Its built in functions made creating plot animations that are live drawn with data from a .csv file much easier. The end result also proved to perform much better.

--- live_data_graphing/bokeh_plot.py
+++ live_data_graphing/bokeh_plot.py
@@ -1,27 +1,62 @@
-import random
 import pandas as pd
-from bokeh.driving import count
+
 from bokeh.models import ColumnDataSource
 from bokeh.plotting import curdoc, figure
-
-fps = 100
-ro = 100
-data = pd.read_csv('data/generated.csv')
-
-source = ColumnDataSource(data)
+from bokeh.layouts import column
 
 
-@count()
-def update(x):
-    y = random.random()
-    source.stream({'x': [x], 'y': [y]}, rollover=ro)
+##############################################
+frame_update_frequency_limit = 50
+frame_period_ms = 1000//frame_update_frequency_limit
+num_data_set_limit = 600
+
+plot_width = 1500
+plot_height = 210
+index_header = 'Packet number'  # also x label
+col_headers = [['Gyroscope X (deg/s)',
+                'Gyroscope Y (deg/s)',
+                'Gyroscope Z (deg/s)', ],
+               ['Accelerometer X (g)',
+                'Accelerometer Y (g)',
+                'Accelerometer Z (g)', ],
+               ['Magnetometer X (G)',
+                'Magnetometer Y (G)',
+                'Magnetometer Z (G)'], ]
+plot_titles = ['Gyroscope', 'Accelerometer', 'Magnetometer']
+plot_y_labels = ['Angle Acceleration', 'Acceleration', 'Magnetic Field']
+line_labels = ['x', 'y', 'z']
+line_colors = ['red', 'green', 'blue']
+
+data_file_path = 'data/generated.csv'
+plotted = 1
+source = ColumnDataSource(pd.read_csv(data_file_path, index_col=index_header))
+##############################################
 
 
-plot = figure()
-plot.line('x', 'y', source=source)
-plot.xaxis.axis_label = 'x'
-plot.yaxis.axis_label = 'y'
+def update():
+    global plotted
+    data = pd.read_csv(data_file_path, index_col=index_header, skiprows=range(1, plotted))
+    source.stream(data, rollover=num_data_set_limit)
+    plotted += len(data)
 
-doc = curdoc()
-doc.add_root(plot)
-doc.add_periodic_callback(update, fps)
+
+def main():
+    plots = []
+    for i in range(len(col_headers)):
+        plots.append(figure(plot_width=plot_width,
+                            plot_height=plot_height,
+                            title=plot_titles[i],
+                            x_axis_label=index_header,
+                            y_axis_label=plot_y_labels[i]))
+        for j in range(3):
+            plots[i].line(index_header,
+                          col_headers[i][j],
+                          line_color=line_colors[j],
+                          legend_label=line_labels[j],
+                          source=source)
+    doc = curdoc()
+    column_to_add = column(plots, sizing_mode='scale_both')
+    doc.add_root(column_to_add)
+    doc.add_periodic_callback(update, frame_period_ms)
+
+main()
